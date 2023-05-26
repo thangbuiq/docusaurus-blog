ae9e95437fd02d73ea4b69bc4fcde6557d23b084
thangbuiq <21280048@student.hcmus.edu.vn>
2023-05-26 20:38:17 +0700

    @thangbuiq - First change on my blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/thangbuiq/docusaurus-blog',
+            'https://github.com/thangbuiq/docusaurus-blog/tree/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/thangbuiq/docusaurus-blog',
+            'https://github.com/thangbuiq/docusaurus-blog/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
